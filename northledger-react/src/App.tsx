53a81606c7970ba556a9b717a4c9cddf26e17b22
CHAITANYAGANDI <chaitanyagandi2000@gmail.com>
2026-07-14 10:15:20 -0400

    add updated protected route integration

--- northledger-react/src/App.tsx
+++ northledger-react/src/App.tsx
@@ -8,7 +8,7 @@ function App(){
   return(
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />}/>
+        <Route path="/" element={<LoginPage/>}/>
         <Route path="/dashboard" element={
           <ProtectedRoute>
             <DashboardPage/>
